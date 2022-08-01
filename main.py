6812a83673f3ce86639bedffb6dfff7df4b9e257
laksh <lakshyam38@gmail.com>
2022-08-01 11:15:55 +0530

    Add OAuth2 Basic auth

--- main.py
+++ main.py
@@ -1,8 +1,69 @@
-from fastapi import FastAPI
+import pathlib
+
+from typing import List
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+from routers import items, auth
 
 app = FastAPI()
+
+# Routers
+app.include_router(items.router, prefix="/items", tags=["items (protected)"])
+app.include_router(auth.router, tags=["auth"])
+
+BASE_DIR = pathlib.Path(__file__).parent
+
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.get("/", response_class=HTMLResponse)
+def sign_in(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@app.get("/feed", response_class=HTMLResponse)
+async def get(request: Request):
+    return templates.TemplateResponse("chat.html", {"request": request})
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
+
 
 @app.get("/healthcheck")
 def health_check():
     return {"msg": "OK"}
-
